803a7b340d939b3b86f69be1c7f68df622abc058
Adrienius01 <adrianthibaud6@gmail.com>
2025-10-04 12:02:25 -0600

    FirebaseFuncional1
    Yeppie

--- firebasecriatura.js
+++ firebasecriatura.js
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLEHOLOGY!</title>
-</head>
-
-<body>
-    
-<script type="module">
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
   import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+
+  console.log("HI! (before error)");
+
   // Your Firebase config
   const firebaseConfig = {
     apiKey: "AIzaSyCIU-q88QGBqNmydsvnB_HaoONK1SNEAzE",
@@ -22,17 +14,24 @@
     appId: "1:166464430045:web:77566d918ba41c43c86034",
     measurementId: "G-T2T1QWYQ2"
   };
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+
+  // Get reference to Realtime Database
   const database = getDatabase(app);
   const rootRef = ref(database);
+
+  console.log("Firebase initialized!", rootRef);
+
+//Create a reference to any node (e.g. "items")
   const itemsRef = ref(database, 'items');
+
   //Push data to your new database
   const newItemRef = push(itemsRef);
-  set(newItemRef, {name: "Coso", createdAt: new Date().toISOString()})
-  .then(() => console.log("✅ Funcioona"))
+  set(newItemRef, {
+    name: "First test item",
+    createdAt: new Date().toISOString()
+  })
+  .then(() => console.log("✅ Data pushed to new DB!"))
   .catch((err) => console.error("❌ Error:", err));
-</script>
-
-</body>
-</html>
